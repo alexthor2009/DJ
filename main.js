8940371a8ec50e5c59d11ef65604ecd8dbd122ea
alexthor2009 <113790589+alexthor2009@users.noreply.github.com>
2023-08-22 11:48:24 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -42,7 +42,34 @@ function draw(){
         document.getElementById("volume").innerHTML="Volume= "+volume;
         song.setVolume(volume);  
     }
-    
+    if(scoreRightWrist>0.2)
+    {
+     if(rightWristY > 0 && rightWristY <=100)
+     {
+        document.getElementById("speed").innerHTML="Speed = 0.5x";
+        song.rate(0.5);
+     }
+     else  if(rightWristY > 100 && rightWristY <=200)
+     {
+        document.getElementById("speed").innerHTML="Speed = 1x";
+        song.rate(1);
+     } 
+     else  if(rightWristY > 200 && rightWristY <=300)
+     {
+        document.getElementById("speed").innerHTML="Speed = 1.5x";
+        song.rate(1.5);
+     } 
+     else  if(rightWristY > 300 && rightWristY <=400)
+     {
+        document.getElementById("speed").innerHTML="Speed = 2x";
+        song.rate(2);
+     } 
+     else  if(rightWristY > 400)
+     {
+        document.getElementById("speed").innerHTML="Speed = 2.5x";
+        song.rate(2.5);
+     } 
+    }
 }
 
 function play(){
@@ -56,6 +83,8 @@ function gotPoses(results){
         console.log(results);
         scoreLeftWrist=results[0].pose.keypoints[9].score;
         console.log("score_leftWrist=" +scoreLeftWrist)
+        scoreRightWrist=results[0].pose.keypoints[10].score;
+        console.log("score_RightWrist=" +scoreLeftWrist)
 
         leftWristX=results[0].pose.leftWrist.x;
         leftWristY=results[0].pose.leftWrist.y;
